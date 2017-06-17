a5c262f017773fe87b9a8d13d061ba1fb8ff6a66
Hal Stroemeria <fripfrop@docomo.ne.jp>
2017-06-18 01:29:21 +0900

    mysqlの文字コードをutf8に変更

--- docker/DBinit/list.sql
+++ docker/DBinit/list.sql
@@ -1,5 +1,10 @@
 
 use my_app;
-create table to_do_lists(list_id int,name text);
-create table Todo(todo_id int,list_id int,text text,lim date);
-insert into to_do_lists value(1,"sample");
+create table to_do_lists(list_id integer primary key,
+                         name text);
+create table to_do(todo_id int primary key ,
+                  list_id int,
+                  text text,
+                  lim date);
+insert into to_do_lists values(1,"チュートリアル");
+insert into to_do values(1,1,"新しいToDoを追加してみましょう。",DATE(NOW()));
