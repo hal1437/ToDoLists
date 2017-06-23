2feb48c765947c0c2d7d3d0ca72f42091e45b27a
Hal Stroemeria <fripfrop@docomo.ne.jp>
2017-06-23 16:14:09 +0900

    検索リストのテストを作成

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -11,6 +11,29 @@ class SearchController extends AppController
 	}
 	public function index(){
 		$this->set('title','ToDoLists - Search');
+// 		$this->Search($this->request->query('page'));
+		$this->Search("レポート");
+	}
+
+	//検索
+	public function Search($key_word){
+		$lists = TableRegistry::get('ToDoLists');
+		$todos = TableRegistry::get('ToDos');
+		
+		//リストの検索
+		$list_query = $lists->find('all',[
+			'conditions' => [
+				'name LIKE' => $key_word
+			]
+		]);
+		//todoの検索
+		$todo_query = $todos->find('all',[
+			'conditions' => [
+				'text LIKE' => $key_word
+			]
+		]);
+		$this->set('list_query',$list_query);
+		$this->set('todo_query',$todo_query);
 	}
 }
 
--- src/Template/Search/index.ctp
+++ src/Template/Search/index.ctp
@@ -3,5 +3,19 @@
 		<h1>検索</h1>
 		<p class="lead"><?= $title?></p>
 	</div>
+	<!--ToDoリストの表示-->
+	<h3><?= $list_query->count() ?>件のリスト</h3>
+	<?php foreach($list_query as $row):?>
+	<?= $this->element('ToDoList',[ 'list_id'  =>$row->getID(),
+									'list_name'=>$row->getName(),
+									'todo_num' =>$row->getToDoCount(),
+									'comp_num' =>$row->getCompToDoCount()]); ?>
+	<?php endforeach?>
+	<h3><?= $todo_query->count() ?>件のToDo</h3>
+	<?php foreach($todo_query as $row):?>
+		<?= $this->element('ToDo',['todo_text'=>$row->getText(),
+									'todo_lim'=>$row->getDate(),
+									'todo_comp'=>$row->isComplete()]); ?>
+	<?php endforeach?>
 </div>
 
